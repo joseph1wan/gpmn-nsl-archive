859e80ae19a9f0795372d0659ca13bce5cbf82b9
Joseph Wan <jhwan@us.ibm.com>
2020-06-07 22:14:34 -0500

    fix: move instructions to datastore. Confirmed with Mike that the DB interface connects with the database and then uses functions defined in the psql package to interact with the database.

--- server/datastore/datastore.go
+++ server/datastore/datastore.go
@@ -4,9 +4,20 @@ import (
 	"time"
 )
 
-// Placeholder while the DB struct gets implemented
+/* DB provides functions to interact with the postgres database. Interactions
+are defined in the psql package */
 type DB interface {
-	Init() error
+	/* Implement the Init() function that connects to the postgres DB */
+	Init() error // Modify this function
+
+	/* Instructions */
+
+	  /* Create a config variable to store your postgres credentials and connection
+	  options. Should use variables from the config.yml file loaded in main.go
+	  (so you need to pass that into Init) */
+
+	  /* Connect to the postgres DB using pgx and return the connection so that the
+	  app can call this interface to connect to the DB */
 }
 
 // Struct to represent the maintenance_request table
