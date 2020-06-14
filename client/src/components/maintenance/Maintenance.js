8c137e705a3eea7a4d31967338d548043c1b02be
Josh Mun <joshuamun@gmail.com>
2020-06-13 19:26:34 -0500

    check in

--- client/src/components/maintenance/Maintenance.js
+++ client/src/components/maintenance/Maintenance.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { Jumbotron, Container } from 'react-bootstrap';
 import Table from '../table';
 
+import './maintenance.scss';
+
 const Maintenance = () => {
   return (
     <React.Fragment>
+      <div className="banner text-center">
+        <h1 className="banner--title text-light font-weight-light">
+          North Star Lodge
+        </h1>
+      </div>
       <Jumbotron fluid>
         <Container>
           <h1 className="display-4">Maintenance Requests</h1>
--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Navbar as BSNavbar, Nav } from 'react-bootstrap';
+import logo from '../../assets/logo.png';
 
 const NavBar = () => {
   return (
     <BSNavbar bg="dark" variant="dark" expand="sm">
-      <BSNavbar.Brand href="/">North Star Lodge</BSNavbar.Brand>
+      <BSNavbar.Brand href="/">
+        <img
+          src={logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top mr-3"
+          alt="North Star Lodge Logo"
+        />
+        North Star Lodge
+      </BSNavbar.Brand>
       <BSNavbar.Toggle aria-controls="basic-navbar-nav" />
       <BSNavbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/maintenance">Maintenance</Nav.Link>
-        </Nav>
         <Nav className="ml-auto">
-          <Nav.Link href="/sign-out">Sign Out</Nav.Link>
+          <Nav.Link href="/info">Info</Nav.Link>
+          <Nav.Link href="/maintenance">Maintenance Requests</Nav.Link>
+          <Nav.Link href="/exit-checklist">Exit Checklist</Nav.Link>
+          <Nav.Link href="/sign-out">Log Out</Nav.Link>
         </Nav>
       </BSNavbar.Collapse>
     </BSNavbar>
