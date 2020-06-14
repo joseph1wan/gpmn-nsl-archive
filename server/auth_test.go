13f234af937a035f488e0b1b93bb399ab6d6f405
Michael Lu <mike.lu.92@gmail.com>
2020-06-13 19:17:40 -0500

    Add more tests

--- server/auth_test.go
+++ server/auth_test.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -32,4 +33,38 @@ func TestLoginReturnsStatusOK(t *testing.T) {
 	assert.Equal(t, http.StatusOK, w.Code) //t,
 	resp, err := ioutil.ReadAll(w.Body)
 	assert.Equal(t, "{\"authToken\":\"ZGFuQGFkbWluLmNvbTphZG1pbg==\",\"userID\":1}", string(resp))
+}
+
+func TestLoginReturnsStatusBadRequest(t *testing.T) {
+	app := setupTestApp()
+
+	w := httptest.NewRecorder()
+
+	req, err := http.NewRequest("POST", "/login", strings.NewReader("bad data"))
+	require.NoError(t, err)
+	app.server.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestLoginReturnsStatusUnauthorized(t *testing.T) {
+	app := setupTestApp()
+
+	w := httptest.NewRecorder()
+
+	reqUser := LoginRequest{
+		Email:    "dan@admin.com",
+		Password: "badpw",
+	}
+
+	reqBytes, err := json.Marshal(reqUser)
+	reqBody := bytes.NewReader(reqBytes)
+
+	req, err := http.NewRequest("POST", "/login", reqBody)
+	require.NoError(t, err)
+	app.server.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusUnauthorized, w.Code) //t,
+	resp, err := ioutil.ReadAll(w.Body)
+	assert.Equal(t, "{\"error\":\"bad user/pw combo\"}", string(resp))
 }
